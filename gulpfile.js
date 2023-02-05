f2fa6bdde40e0a4a76a47549da1d2bd3b006629b
rulee.chen <rulee@live.cn>
2023-02-05 18:40:42 +0800

    fix(pio): build data deps

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,51 @@ const del = require("del");
 const gulp = require("gulp");
 const ignore = require("ignore");
 
-const PATH_ROOT = path.resolve(__dirname, "./");
-const PATH_DEPS = path.resolve(__dirname, "./.pio/libdeps");
-
-const DIR_DATA = "data";
-const DIR_DATA_WEB = path.resolve(DIR_DATA, "web");
-
-const ig = ignore();
-ig.add(fse.readFileSync(path.join(__dirname, ".gitignore")).toString());
-
+// argv
 const {
   argv: { pioEnv },
 } = yargs.string("pioEnv");
 
+// paths
+const DIR_PACKAGES = "packages";
+const DIR_DATA = "data";
+
+const PATH_DEPS = path.resolve(__dirname, "./.pio/libdeps", pioEnv);
+const PATH_PROJ = path.resolve(__dirname, DIR_PACKAGES, pioEnv);
+
+const PATH_DATA = path.resolve(PATH_PROJ, DIR_DATA);
+const PATH_DATA_WEB = path.resolve(PATH_PROJ, DIR_DATA, "web");
+const PATH_MAIN = path.resolve(PATH_PROJ, "src/main.cpp");
+
+console.log(`********************
+* [env:${pioEnv}]
+*
+* deps ${PATH_DEPS}
+* proj ${PATH_PROJ}
+* main ${PATH_MAIN}
+*
+********************`);
+
+// ignores
+const FILE_GITIGNORE = ".gitignore";
+const PATH_ROOT_IG = path.join(__dirname, FILE_GITIGNORE);
+const ig = ignore();
+ig.add(fse.readFileSync(PATH_ROOT_IG).toString());
+
 function clean() {
-  return del([DIR_DATA_WEB], { force: true });
+  return del([PATH_DATA_WEB], { force: true });
 }
 
 function buildDeps() {
   if (fse.existsSync(PATH_DEPS)) {
-    const libDir = path.resolve(PATH_DEPS, pioEnv, "home-esp8266", DIR_DATA);
+    const libDir = path.resolve(PATH_DEPS, "home-esp8266", DIR_DATA);
     if (fse.existsSync(libDir)) {
-      const destination = path.resolve(PATH_ROOT, "packages", pioEnv, DIR_DATA);
-      fse.copySync(libDir, destination, {
+      const refDataPath = path.relative(__dirname, PATH_DATA);
+      fse.copySync(libDir, PATH_DATA, {
         overwrite: true,
         filter: (src, dest) => {
           const relDest = path.relative(__dirname, dest);
-          return relDest === DIR_DATA || ig.ignores(relDest);
+          return relDest === refDataPath || ig.ignores(relDest);
         },
       });
     }
