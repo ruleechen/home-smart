1b7507fcb71a8083991c15b26f0f88362857a1dd
rulee.chen <rulee@live.cn>
2025-11-20 09:32:23 +0800

    fix(build): load lib failed

--- scripts/pre_script.py
+++ scripts/pre_script.py
@@ -2,33 +2,35 @@ Import("env")
 import os
 
 def get_package_name():
-    try:
-        custom_package = env.GetProjectOption("custom_package", None)
-        if custom_package:
-            print(f"Using custom package: {custom_package}")
-            return custom_package
-    except:
-        pass
-    return env["PIOENV"]
+  try:
+    custom_package = env.GetProjectOption("custom_package", None)
+    if custom_package:
+      print(f"Using custom package: {custom_package}")
+      return custom_package
+  except:
+    pass
+  return env["PIOENV"]
 
 def update_project_paths():
-    package_name = get_package_name()
-    base_paths = {
-        'PROJECT_SRC_DIR': f"packages/{package_name}/src",
-        'PROJECT_LIB_DIR': f"packages/{package_name}/lib",
-        'PROJECT_INCLUDE_DIR': f"packages/{package_name}/include",
-        'PROJECT_DATA_DIR': f"packages/{package_name}/data",
-        'PROJECT_TEST_DIR': f"packages/{package_name}/test",
-        'PROJECT_BOARDS_DIR': f"packages/{package_name}/boards",
-    }
-    # root project directory
-    project_dir = env.get("PROJECT_DIR")
-    # update env variables
-    for key, relative_path in base_paths.items():
-        full_path = os.path.join(project_dir, relative_path)
-        if os.path.exists(full_path):
-            env[key] = full_path
-            print(f"Updated {key}: {relative_path}")
+  package_name = get_package_name()
+  # define base paths
+  lib_dir = f"packages/{package_name}/lib"
+  base_paths = {
+    'PROJECT_SRC_DIR': f"packages/{package_name}/src",
+    'PROJECT_DATA_DIR': f"packages/{package_name}/data",
+    'PROJECT_INCLUDE_DIR': f"packages/{package_name}/include",
+    'PROJECT_TEST_DIR': f"packages/{package_name}/test",
+  }
+  # root project directory
+  project_dir = env.get("PROJECT_DIR")
+  # include lib directory
+  env.Prepend(LIBSOURCE_DIRS=[os.path.join(project_dir, lib_dir)])
+  # update env variables
+  for key, relative_path in base_paths.items():
+    full_path = os.path.join(project_dir, relative_path)
+    if os.path.exists(full_path):
+      env[key] = full_path
+      print(f"Updated {key}: {relative_path}")
 
 # execute
 update_project_paths()
